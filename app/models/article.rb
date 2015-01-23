e93fc6ed7439855e95eb2bc74d381ffda5eb9c09
Johnny Ji <johnny.ji@live.ca>
2015-01-23 15:23:19 -0800

    finished login system, added more css styling and fixed github bug

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ActiveRecord::Base
 	has_many :comments
+
+	has_attached_file :image
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+	has_attached_file :image, styles: { medium: "400x400>", thumb: "100x100>" }
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'author/session'
+
   resources :articles do
     resources :comments
   end
 
   root "articles#index"
 
+  resources :authors
+  resources :author_sessions, only: [ :new, :create, :destroy]
+
+  get 'login' => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 end
