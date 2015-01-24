837db0492d72bb4bfc22b0934d1f6573d9c3f950
Johnny Ji <johnny.ji@live.ca>
2015-01-23 16:53:43 -0800

    added additional css styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,13 +3,14 @@ include ArticlesHelper
 before_filter :require_login, only: [:new, :create, :edit, :update, :destroy]
 
 	def index
-		@articles = Article.all.order('created_at DESC');
+		@articles = Article.all.order('created_at DESC')
 	end
 
 	def show
 		@article = Article.find(params[:id])
 		@comment = Comment.new
 		@comment.article_id = @article_id
+		@comments = @article.comments.order('created_at DESC')
 	end
 
 	def new
