b82e47237acfd7d2076f4d833b8d1ad0f6041aa8
Johnny Ji <johnny.ji@live.ca>
2015-04-12 00:18:44 -0700

    added dyno file

--- config/initializers/dynopoker.rb
+++ config/initializers/dynopoker.rb
@@ -1,5 +1,5 @@
 Dynopoker.configure do |config|
-    config.address = 'http://wakemydyno.com'
+    config.address = 'http://johnnyji.me'
 #  config.enable = false # default is true
 #  config.poke_frequency = 123 # default is 1800s (30min)
 end
